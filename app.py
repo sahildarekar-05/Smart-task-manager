a27b43f13a07e3a7addcf2c6c0ed457084b48a73
Sahil Darekar <work.sahildarekar@gmail.com>
2026-05-14 16:23:00 +0530

    Fixed Render port binding

--- app.py
+++ app.py
@@ -50,12 +50,12 @@ def dashboard():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    
-    port = int(os.environ.get('PORT', 5000))
 
+    port = int(os.environ.get('PORT', 5000))
     socketio.run(
         app,
         host='0.0.0.0',
         port=port
     )
+
 
