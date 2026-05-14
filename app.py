fd55bb1ac4b581867d32ae070840568669a3a0b3
Sahil Darekar <work.sahildarekar@gmail.com>
2026-05-14 17:02:06 +0530

    Fixed model imports for database creation

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os 
+
 from flask import Flask
 from flask import render_template
 from extensions import db, login_manager, socketio
--- config.py
+++ config.py
@@ -1,7 +1,15 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        'DATABASE_URL',
+        'postgresql://postgres:admin123@localhost/taskmanager '
+    )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = os.getenv(
+        'SECRET_KEY',
+        'smart_task_secret'
+    )
