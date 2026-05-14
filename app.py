9317ca5277dee48d971a19ef153291a440eb74c6
Sahil Darekar <work.sahildarekar@gmail.com>
2026-05-14 16:14:05 +0530

    Fixed render deployment

--- app.py
+++ app.py
@@ -50,5 +50,12 @@ def dashboard():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    socketio.run(app, debug=True)
+    
+    port = int(os.environ.get('PORT', 5000))
+
+    socketio.run(
+        app,
+        host='0.0.0.0',
+        port=port
+    )
 
